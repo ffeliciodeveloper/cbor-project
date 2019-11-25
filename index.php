4ae6982f5b50dbdbc037023c201ca710873c7261
Fernando Felício <fernando_felicio82@yahoo.com.br>
2019-11-25 16:10:12 -0300

    Initial commit

--- index.php
+++ index.php
@@ -1,15 +1,40 @@
 <?php
 
+require __DIR__.'/vendor/autoload.php';
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-
-require __DIR__ . '/vendor/autoload.php';
+use CBOR\CBOREncoder;
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write('Hello world!');
+$app->get('/cbor', function (Request $request, Response $response, $args) {
+    //target for encode
+    $target = [
+        [2 => ['c' => true]],
+        [3 => ['p' => 10]],
+        1,
+        'string',
+        [1, 2, 3, 4],
+    ];
+
+    //encoded string
+    $encoded_data = CBOREncoder::encode($target);
+
+    //debug info output
+    $byte_arr = unpack('C*', $encoded_data);
+    $dechex = implode(' ', array_map(function ($byte) {
+        $dechex = dechex($byte);
+
+        if (strlen($dechex) == 1 || (is_numeric($dechex) && $dechex < 10)) {
+            $dechex = '0'.$dechex;
+        }
+
+        return '0x'.$dechex;
+    }, $byte_arr));
+
+    $response->getBody()->write($dechex);
 
     return $response;
 });
